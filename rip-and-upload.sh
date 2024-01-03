6bbd7da5e923de4abf30f17176da07e3daa4b06a
Tomy Hsieh <git@tomy.me>
2024-01-04 01:02:25 +0800

    ✨ feat: Upload scripts and configs

---
+++ rip-and-upload.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+TIMESTAMP="$(date '+%Y%m%d_%H%M%S')"
+LOCAL_DIR="/home/${USER}/Music"
+OUTPUTDIR="${TIMESTAMP}"
+DROPBOX_BASE_DIR='Requested/Music'
+
+echo "==========${TIMESTAMP}=========="
+
+# Enter Music directory
+cd "${LOCAL_DIR}"
+
+# Rip CD into flac
+OUTPUTDIR="${OUTPUTDIR}" abcde -N -x -P -o flac
+
+# Upload to Dropbox file by file
+for file in "${OUTPUTDIR}"/*; do
+    # Check if the item is a file
+    echo "Uploading ${file}"
+    if [ -f "${file}" ]; then
+        dbxcli put "${file}" "${DROPBOX_BASE_DIR}/${file}"
+        echo "Done: ${file}"
+    fi
+done
+
+echo "==========${TIMESTAMP}=========="
